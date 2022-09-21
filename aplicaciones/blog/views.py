67a43b8392a5d3bd2e280f5b18b34361817e52d7
onelys <ingelcon1@gmail.com>
2022-09-21 16:52:40 -0400

    commit blog1

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,3 +1,12 @@
+from ast import arg
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class blogListView(View):
+    def get(self,request,*args, **kwargs):
+        contexto={
+            
+        }
+        return render(request, 'blog.html', contexto)
