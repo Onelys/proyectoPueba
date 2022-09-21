6b48e3559ddc15b7a9a09be3d5543a278592fd89
onelys <ingelcon1@gmail.com>
2022-09-21 16:30:15 -0400

    commit blog app

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$=3dpkh$61u(!7$vv3c&9xb(&d$y_d&wr)xgh1_3h!+*t629r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-s$=3dpkh$61u(!7$vv3c&9xb(&d$y_d&wr)xgh1_3h!+*t629r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
