c26f023ac8b2e08f86d1fcce13c8915b96337bc5
onelys <ingelcon1@gmail.com>
2022-09-22 11:19:05 -0400

    actualizar

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -1,3 +1,10 @@
+from atexit import register
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+admin.site.register(Post, P)
