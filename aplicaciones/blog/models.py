7547608e7f6163c3316f69236d690aab6788a828
onelys <ingelcon1@gmail.com>
2022-09-22 11:24:02 -0400

    actualizar

--- aplicaciones/blog/models.py
+++ aplicaciones/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    
+    
